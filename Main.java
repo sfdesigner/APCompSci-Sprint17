4e314df0d5895cb21aa30c1e7d0a2d45e47e9ed0
Doug <doug@sfdesignerdw.com>
2018-09-17 22:35:22 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // I: Define a variable
+        int quantity;   // integer
+        double price;   // double precision decimal
+        String name;    // string, note the capital 'S'
+        char initial;   // character
+
+        // II: Assign a value to a variable using the assignment operator
+        quantity = 5;
+        price = 2.50;
+        name = "Doug";
+        initial = 'W';
+
+        // III: Output a variable
+        System.out.printf("%s %s. bought %d items priced at %f each\n",name,initial,quantity,price);
+        /* Output
+        Doug W. bought 5 items priced at 2.500000 each
+         */
+
+        // IV: Update value of a variable
+        quantity = 10;
+        price = 1.99;
+        name = "Mike";
+        initial = 'J';
+
+        // V: Output updated variables
+        System.out.printf("%s %s. bought %d items priced at $%.2f each\n",name,initial,quantity,price);
+        /* Output
+        Mike J. bought 10 items priced at $1.99 each
+         */
+
+        // VI: Initialize and assign on one line
+        int myNumber = 5;
+
+        // VII: Assign one variable to another
+        int myValue = myNumber;
+    }
+}
